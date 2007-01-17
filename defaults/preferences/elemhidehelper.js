a6d8ad9405f9f1093d61126b8fd0c4b0e1f9d10e
Wladimir Palant <trev@gtchat.de>
2007-01-17 23:25:02 +0000

    Show warning when required ABP version is not installed
    --HG--
    extra : convert_revision : svn%3Ad8bf93c1-8190-44a8-bb31-1ea94378a4df/trunk%40638

--- defaults/preferences/elemhidehelper.js
+++ defaults/preferences/elemhidehelper.js
@@ -1,2 +1,3 @@
 pref("extensions.adblockplus.ehh-selectelement_key", "Accel Shift H");
 pref("extensions.adblockplus.protectchrome.ehh", "elemhidehelper");
+pref("extensions.adblockplus.ehh.norequirementswarning", false);
