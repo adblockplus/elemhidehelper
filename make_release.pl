45b2a797411e8b20c59c2eb0743169c75ec34ec9
Wladimir Palant <trev@adblockplus.org>
2010-10-19 16:43:13 +0200

    Moved extension metadata to a central location. install.rdf is now being generated automatically and setting homepage is no longer up to translators.

--- make_release.pl
+++ make_release.pl
@@ -2,8 +2,6 @@
 
 # This is a dummy, its purpose is to call a script with the same name in the buildtools repository
 
-our $BRANCH_NAME = "ELEMENT_HIDING_HELPER";
-
 $0 =~ s/(.*[\\\/])//g;
 chdir($1) if $1;
 
