223ca1a0d646ddfc1d60a7bc350fe04cf67995f5
Wladimir Palant <trev@adblockplus.org>
2010-10-26 11:29:31 +0200

    Topic 6180 - Composer: Make pre-selected domain configurable

--- defaults/preferences/elemhidehelper.js
+++ defaults/preferences/elemhidehelper.js
@@ -1,3 +1,4 @@
 pref("extensions.elemhidehelper.selectelement_key", "Accel Shift K, Accel Shift S");
 pref("extensions.elemhidehelper.showhelp", true);
 pref("extensions.elemhidehelper.acceptlocalfiles", false);
+pref("extensions.elemhidehelper.composer_defaultDomain", 3);
