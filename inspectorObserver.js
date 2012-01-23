04aaf2a6b3338c7450f3137ef64045b705c9e498
Wladimir Palant <trev@adblockplus.org>
2012-01-23 14:52:53 +0100

    Updated to build tools changes and split up the main module

---
+++ inspectorObserver.js
@@ -0,0 +1,62 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+let InspectorObserver =
+{
+  init: function()
+  {
+    Services.obs.addObserver(this, "inspector-opened", true);
+    onShutdown.add((function() Services.obs.removeObserver(this, "inspector-opened")).bind(this));
+  },
+
+  get inspectorButton()
+  {
+    // Randomize URI to work around bug 719376
+    let stringBundle = Services.strings.createBundle("chrome://elemhidehelper/locale/global.properties?" + Math.random());
+    let result = [stringBundle.GetStringFromName("inspector.button.label"), stringBundle.GetStringFromName("inspector.button.accesskey"), stringBundle.GetStringFromName("inspector.button.tooltiptext")];
+
+    delete this.inspectorButton;
+    this.__defineGetter__("inspectorButton", function() result);
+    return this.inspectorButton;
+  },
+
+  observe: function(subject, topic, data)
+  {
+    if (topic != "inspector-opened")
+      return;
+
+    let InspectorUI = subject.wrappedJSObject;
+    let hooks = InspectorUI.chromeDoc.getElementById("abp-hooks");
+    if (!hooks || !require("aardvark").Aardvark.canSelect(hooks.getBrowser()))
+      return;
+
+    let [label, accesskey, tooltiptext] = this.inspectorButton;
+    InspectorUI.registerTool({
+      id: "abp-elemhide",
+      label: label,
+      accesskey: accesskey,
+      tooltiptext: tooltiptext,
+      get isOpen() false,
+      show: function(selection)
+      {
+        InspectorUI.chromeWin.openDialog("chrome://elemhidehelper/content/composer.xul", "_blank",
+                                         "chrome,centerscreen,resizable,dialog=no", selection);
+        InspectorUI.closeInspectorUI();
+      }
+    });
+  },
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsISupportsWeakReference, Ci.nsIObserver])
+};
+
+InspectorObserver.init();
---
+++ windowWrapper.js
@@ -0,0 +1,156 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+let {Aardvark} = require("aardvark");
+let {Prefs} = require("prefs");
+let {KeySelector} = require("keySelector");
+
+let key = undefined;
+
+function getMenuItem()
+{
+  // Randomize URI to work around bug 719376
+  let stringBundle = Services.strings.createBundle("chrome://elemhidehelper/locale/global.properties?" + Math.random());
+  let result = [stringBundle.GetStringFromName("selectelement.label"), stringBundle.GetStringFromName("stopselection.label")];
+
+  getMenuItem = function() result;
+  return getMenuItem();
+}
+
+exports.WindowWrapper = WindowWrapper;
+function WindowWrapper(wnd, elementMarkerClass)
+{
+  this.window = wnd;
+  this.browser = this.E("abp-hooks").getBrowser();
+
+  this.popupShowingHandler = this.popupShowingHandler.bind(this);
+  this.popupHidingHandler = this.popupHidingHandler.bind(this);
+  this.keyPressHandler = this.keyPressHandler.bind(this);
+  this.toggleSelection = this.toggleSelection.bind(this);
+  this.hideTooltips = this.hideTooltips.bind(this);
+  this.stopSelection = this.stopSelection.bind(this);
+
+  this.E("ehh-elementmarker").firstElementChild.setAttribute("class", elementMarkerClass);
+
+  this.init();
+}
+WindowWrapper.prototype =
+{
+  window: null,
+  browser: null,
+
+  init: function()
+  {
+    this.window.addEventListener("popupshowing", this.popupShowingHandler, false);
+    this.window.addEventListener("popuphiding", this.popupHidingHandler, false);
+    this.window.addEventListener("keypress", this.keyPressHandler, false);
+    this.window.addEventListener("blur", this.hideTooltips, true);
+    this.browser.addEventListener("select", this.stopSelection, false);
+  },
+
+  shutdown: function()
+  {
+    this.window.removeEventListener("popupshowing", this.popupShowingHandler, false);
+    this.window.removeEventListener("popuphiding", this.popupHidingHandler, false);
+    this.window.removeEventListener("keypress", this.keyPressHandler, false);
+    this.window.removeEventListener("blur", this.hideTooltips, true);
+    this.browser.removeEventListener("select", this.stopSelection, false);
+  },
+
+  E: function(id)
+  {
+    let doc = this.window.document;
+    this.E = function(id) doc.getElementById(id);
+    return this.E(id);
+  },
+
+  key: undefined,
+
+  popupShowingHandler: function(event)
+  {
+    let popup = event.target;
+    if (!/^(abp-(?:toolbar|status|menuitem)-)popup$/.test(popup.id))
+      return;
+
+    let enabled = Aardvark.canSelect(this.browser);
+    let running = (enabled && this.browser == Aardvark.browser);
+
+    let [labelStart, labelStop] = getMenuItem();
+    let item = popup.ownerDocument.createElement("menuitem");
+    item.setAttribute("label", running ? labelStop : labelStart);
+    item.setAttribute("class", "elemhidehelper-item");
+    if (!enabled)
+      item.setAttribute("disabled", "true");
+
+    if (typeof key == "undefined")
+      this.configureKey(event.currentTarget);
+    item.setAttribute("acceltext", KeySelector.getTextForKey(key));
+
+    item.addEventListener("command", this.toggleSelection, false);
+
+    let insertBefore = null;
+    for (let child = popup.firstChild; child; child = child.nextSibling)
+      if (/-options$/.test(child.id))
+        insertBefore = child;
+    popup.insertBefore(item, insertBefore);
+  },
+
+  popupHidingHandler: function(event)
+  {
+    let popup = event.target;
+    if (!/^(abp-(?:toolbar|status|menuitem)-)popup$/.test(popup.id))
+      return;
+
+    let items = popup.getElementsByClassName("elemhidehelper-item");
+    if (items.length)
+      items[0].parentNode.removeChild(items[0]);
+  },
+
+  keyPressHandler: function(event)
+  {
+    if (typeof key == "undefined")
+      this.configureKey(event.currentTarget);
+
+    if (KeySelector.matchesKey(event, key))
+    {
+      event.preventDefault();
+      this.toggleSelection();
+    }
+  },
+
+  configureKey: function(window)
+  {
+    key = new KeySelector(window).selectKey(Prefs.selectelement_key);
+  },
+
+  hideTooltips: function()
+  {
+    if (Aardvark.window == this.window)
+      Aardvark.hideTooltips();
+  },
+
+  toggleSelection: function()
+  {
+    if (this.browser == Aardvark.browser)
+      this.stopSelection();
+    else
+      this.startSelection();
+  },
+
+  startSelection: function()
+  {
+    Aardvark.start(this);
+  },
+
+  stopSelection: function()
+  {
+    Aardvark.quit();
+  }
+};
