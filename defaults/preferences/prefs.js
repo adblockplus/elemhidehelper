98130cb3aa846f53e723407ce14b52c3e892a826
Wladimir Palant <trev@adblockplus.org>
2012-03-09 17:06:14 +0100

    Added Ctrl-Shift-F3 as possible shortcut key, the new Debugger tool ate Ctr-Shift-S (see bug 697762)

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,4 @@
-pref("extensions.elemhidehelper.selectelement_key", "Accel Shift K, Accel Shift S");
+pref("extensions.elemhidehelper.selectelement_key", "Accel Shift K, Accel Shift S, Accel Shift F3");
 pref("extensions.elemhidehelper.showhelp", true);
 pref("extensions.elemhidehelper.acceptlocalfiles", false);
 pref("extensions.elemhidehelper.composer_defaultDomain", 3);
